a79612dd50f8e82fe804c50d43cc1cd87abff24f
Vamsi Vidala <vamsi.vidala@forrent.com>
2014-02-06 14:07:58 -0500

    Removeing resources

--- Resources/android/entity.js
+++
@@ -1,4 +0,0 @@
-function Entity(name) {
-    this.name = name;
-    this.health = 100;
-}
--- Resources/iphone/entity.js
+++
@@ -1,6 +0,0 @@
-function Entity(name) {
-    this.name = name;
-    this.health = 100;
-    this.attack = 10;
-    this.defense = 2;
-}
