f5e74d90227d7ed272738e736c63a0b0d0898188
Daniel Burgner <dburgnerjr@yahoo.com>
2014-02-06 21:05:44 -0500

    added enemies list

--- app/controllers/game.js
+++ app/controllers/game.js
@@ -4,13 +4,36 @@ var Entity = require('entity');
 var Spells = require('spell');
 
 var player = new Entity(args);
-var enemy = new Entity("Evil guy");
+var enemies = [];
 
+function loadEnemies() {
+	var url = "https://quasar-9.herokuapp.com/api/v1/job_postings?auth_token=1pSst1P7LAQBzNGc2bgW&site_of_origin=EG&q=java&employer_id=1526";
+	var client = Ti.Network.createHTTPClient({
+    	 // function called when the response data is available
+	     onload : function(e) {
+    	     var json = JSON.parse(this.responseText);
+    	     for (var i = 0; i < json.job_postings.length; i++) {
+    	     	enemies.push(new Entity.parse(json.job_postings[i]));
+    	     }
+	     },
+    	 // function called when an error occurs, including a timeout
+	     onerror : function(e) {
+    	     Ti.API.debug(e.error);
+        	 alert('error');
+	     },
+    	 timeout : 5000  // in milliseconds
+	 });
+ 	 // Prepare the connection.
+ 	 client.open("GET", url);
+	 // Send the request.
+	 client.send();
+}
+ 
 function onAttackClick(e){
 	
 	e.source.backgroundImage = "images/attackPrs.jpg";
 	
-	player.attack(enemy);
+	//player.attack(enemy);
 	
 	e.source.backgroundImage = "images/attackBtn.jpg";
 	
@@ -29,7 +52,7 @@ function onDefendClick(e){
 }
 
 function enemyTurn(){
-	enemy.act(player);
+	//enemy.act(player);
 }
 
 $.btnAttack.addEventListener('click', onAttackClick);
