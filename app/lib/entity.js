be23d7e52126594ad912bd74cea255dbf83d56f2
Austin Krill <AKrill91@gmail.com>
2014-02-06 22:54:11 -0500

    Enemy loading

--- app/lib/entity.js
+++ app/lib/entity.js
@@ -6,13 +6,13 @@ function Entity(name)
 var Player = function(name, skills){
 	Entity.call(this, name);
 	this.skills = skills;
-	this.available_skills = skills;
 };
 
 Player.prototype = Object.create(Entity.prototype);
 Player.prototype.constructor = Player;
 
 var Enemy = function(job_posting){
+	Ti.API.info("Enemy constructor called.\n", JSON.stringify(job_posting));
 	Entity.call(this, job_posting.title);
 	this.source_url = job_posting.source_url;
 	this.apply_url = job_posting.apply_url;
@@ -24,7 +24,6 @@ Enemy.prototype.constructor = Enemy;
 
 Player.prototype.useSkill = function(skill, target)
 {
-	this.available_skills.pop(skill);
 	return target.description.match(skill) ? 1 : -1;
 };
 
