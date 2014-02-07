e003dc1f8889f7aa835aec837ab18aa9bfa70dc6
Allen T <atorres757@gmail.com>
2014-02-06 23:10:38 -0500

    Adding linkedIn lib.

---
+++ app/lib/linkedIn/linkedInAPI.js
@@ -0,0 +1,28 @@
+/*
+ This is a minified javascript module because I have not had a chance to refactor and
+ clean up the horrible hacks I have done to make this work, and I don't want to expose
+ the code and then spend alot of time responding to issues and/or questions.
+ 
+ This code is released AS IS and I will clean it up, document it better and eventually
+ re-release it as a module or open source
+ 
+*/
+var linkedInModule = require('linkedInMod');
+
+//
+// MUST INITIALIZE WITH KEYS
+// GET KEYS AT https://developer.linkedin.com/apis
+//
+linkedInModule.init('tCTSMKiSV8XznPdD', '77klx3f1fgcdnb');
+    
+
+// THESE CALLS WRAP COMMON FUNCTIONS
+linkedInModule.getUser(function(_d) {
+    Ti.API.info(_d);
+});
+
+Alloy.Globals.liCall = function() {
+    linkedInModule.getUser(function(_d) {
+        Ti.API.info(_d);
+    });
+};
